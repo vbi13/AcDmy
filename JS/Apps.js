5b93797b788981bdd400d6bbb46153345a933911
vbi13 <fabiankulesza13@gmail.com>
2022-03-06 21:02:21 +0100

    main

---
+++ JS/Apps.js
@@ -0,0 +1,87 @@
+//1
+
+//const name = 'Jan'
+//const age = 22
+//let isOpen = true
+//const stringNumber = 100
+
+//2
+
+//console.log(10%4);
+//const name = 'Tomasz'
+//const age = 33
+
+//console.log(`${name} ma ${age} lata`);
+
+//const firstName = 'Jan'
+//const lastName = 'Kowalski'
+
+//console.log(`${firstName} ${lastName}`);
+//console.log('Jan'+ ' '+ 'Kowalski');
+
+//3
+
+//const number = [1,6]
+//console.log(`wynik=${number[0]+number[1]}`);
+
+//4
+
+//const arrayOfNumbers = [1,2,3]
+//const arrayOfCars = ['BMW','Audi','Hyundai']
+//const isTrue = [true, false, true]
+//const arrayIsEmpty = []
+//const arrayOfAll = [1, 'woda', true, undefined, null]
+//console.log(arrayOfAll[1]);
+
+// ***
+
+//const names = ['Adrian', 'Maciek', 'Fabian']
+//console.log(names.length);
+//***
+
+//const arrayofData =['person1','person2','person3']
+//const person1 = {
+//name:'Adrian',
+//surname: 'Kopytko',
+//age: 43,
+//};
+//const person2 = {
+//name:'Paulina',
+//surname:'Karp',
+//age: 28,
+//}
+//const person3 = {
+//name: 'Weronika',
+//surname:'Byk',
+//age: 54,
+//}
+//console.log(person2.name);
+
+//5
+//const object = {};
+//const person =
+//{
+//      name: 'Iza',
+//};
+//const personDetails =
+//{
+//      name: 'Weronika',
+//    lastName: 'Kulesza',
+//  age: 29,
+// drivingLicense: true,
+//}
+//console.log(personDetails.age);
+
+//const dataOfCar = {
+//      brand: 'Toyota',
+//    color: 'yellow',
+//  dateOfProd: 2000,
+//  reg: []
+//};
+//let regData = [2002,2005,2020];
+
+// dataOfCar.reg.push(regData);
+
+// console.log(dataOfCar);
+
+//6
